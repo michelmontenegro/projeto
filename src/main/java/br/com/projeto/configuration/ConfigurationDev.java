bcbce1d6c2ea42313b58bf5da7665334c0d25267
Michel Montenegro <michel.montenegro@gmail.com>
2023-04-12 17:52:20 -0300

    Subindo apresentação e atualizandoa tela.

---
+++ src/main/java/br/com/projeto/configuration/ConfigurationDev.java
@@ -0,0 +1,46 @@
+package br.com.projeto.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+/**
+ * Acessar Base de Dados
+ * http://localhost:8080/h2-console
+ *
+ * Banco executa somente na memoria, enquanto a aplicação esta ativa
+ */
+@Configuration
+@Profile("dev")
+public class ConfigurationDev {
+
+    @Bean
+    public DataSource dataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("password");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter()
+    {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.H2);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+
+}
